7af3e93cf9634b2f84822abdf5254d97987f9afd
unknown <borys.vengerov@gmail.com>
2016-02-06 22:23:26 +0300

    Moved CSS to stylesheet; Added dummy router; Removed unnecessary old stuff

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 (function() {
 
-	var app = angular.module('borrowsimply', []);
+	var app = angular.module("borrowsimply", ["ngRoute"]);
 
-	var $baseUrl = 'services/';
+	var $baseUrl = "services/";
 
-	app.factory('itemsService', function($http) {
+	app.factory("itemsService", function($http) {
 		return {
 			getItems: function() {
-				return $http.get($baseUrl + 'getItems.php');
+				return $http.get($baseUrl + "getItems.php");
 			},
 			takeItem: function(id) {
 				$http({
-				    method: 'POST',
-				    url: $baseUrl + 'takeItem.php',
-				    data: 'id' + id,
-				    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				    method: "POST",
+				    url: $baseUrl + "takeItem.php",
+				    data: "id" + id,
+				    headers: {"Content-Type": "application/x-www-form-urlencoded"}
 				});
-				//return $http.post($baseUrl + 'takeItem.php', {'id': id});
+				//return $http.post($baseUrl + "takeItem.php", {"id": id});
 			}
 		};
 	});
 
-	app.controller('ItemController', function(itemsService) {
+	app.controller("ItemController", function(itemsService) {
 
 		var that = this;
 
@@ -36,10 +36,20 @@
 			//TODO reloading for all the clients
 			//TODO add changing the real status in MySQL
 			//TODO add timepoint to the string
-			//TODO add a check for whether the item has really been taken
+			//TODO add a check for whether the item has really been taken via SQL transaction
 			//item.status = "Taken by vengerov";
 		//	itemsService.takeItem();
 		//};
+	});
+
+	app.config(function($routeProvider){
+		$routeProvider
+		.when("/main", {
+			templateUrl: "main.html",
+			controller: "MainController",
+			controllerAs: 'MainCtrl'
+		})
+		.otherwise({redirectTo: "/main"})
 	});
 })();
 
--- json.php
+++
@@ -1,7 +0,0 @@
-<?php
-	namespace Home;
-	require_once("DbObj.php");
-
-	$db = new DbObj;
-	$json = json_encode($db->getRows());
-?>
