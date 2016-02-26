09a1b40c5a932fb5985eb1783502193549561909
unknown <borys.vengerov@gmail.com>
2016-02-26 07:35:39 +0300

    Moved ItemsService out of app.js

--- app.js
+++ app.js
@@ -2,55 +2,6 @@
 
 	var app = angular.module("borrowsimply", ['ngCookies', 'ngQtip2']);
 
-	var $baseUrl = "services/";
-
-	app.factory("itemsService", function($http) {
-		return {
-			getItems: function() {
-				return $http({
-				    method: "GET",
-				    url: $baseUrl + "getItems.php"
-				});
-			},
-			takeItem: function(item, username) {
-				return $http({
-				    method: "POST",
-				    url: $baseUrl + "takeItem.php",
-					 data: {
-					 	id: item.id,
-					 	status: "Taken by " + username
-					 },
-					 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-				});
-			},
-			returnItem: function(item) {
-				return $http({
-				    method: "POST",
-				    url: $baseUrl + "returnItem.php",
-					data: item.id,
-					headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-				});
-			},
-			updateComment: function(item, comment) {
-				return $http({
-				    method: "POST",
-				    url: $baseUrl + "updateComment.php",
-					data: {
-						'id': item.id,
-						'comment': comment
-					},
-					headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-				});
-			},
-			getUsers: function() {
-					return $http({
-				    method: "GET",
-				    url: $baseUrl + "getUsers.php"
-				});
-			}
-		};
-	});
-
 	app.controller("ItemController", function($scope, itemsService, $interval, $window, $cookies) {
 
 		var getItems = function() {
